dd0db02253c00c1f110d6783599206be46cfe5b2
zhixingheyixin <38305313+zhixingheyixin@users.noreply.github.com>
2019-01-23 17:29:31 +0800

    Update TestApplication.java
    启动日志

--- src/main/java/com/liang/TestApplication.java
+++ src/main/java/com/liang/TestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test启动成功");
 		SpringApplication.run(TestApplication.class, args);
 	}
 
